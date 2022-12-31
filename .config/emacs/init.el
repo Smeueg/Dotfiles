26bedd27a8097f8d4a3e676710ae65bff8b81271
Smeueg <Smeueg@gmail.com>
2022-12-31 21:15:39 +0700

    Fixed w()

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -35,8 +35,8 @@
 
 (defun w ()
   (interactive)
-  "Save a buffer if modified or finish an edit (i.e. commit message)"
-  (if (string= "COMMIT_MSG" (file-name-base (or (buffer-file-name) "")))
+  "Save a buffer if modified or finish an edit (i.e. a commit message)"
+  (if (string= "COMMIT_EDITMSG" (file-name-base (or (buffer-file-name) "")))
       (call-interactively 'with-editor-finish)
     (call-interactively 'save-buffer)))
 
