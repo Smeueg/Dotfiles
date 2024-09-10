79438d3a0f277fd21bbdd64c972e046087ddcfc2
Smeueg <Smeueg@gmail.com>
2024-09-10 17:57:12 +0700

    emacs: Changed the colors for when Emacs first starts up

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -2,8 +2,8 @@
 (scroll-bar-mode 0) ;; Disable the scrollbar
 ;; Set the colors
 (set-face-attribute 'default nil
-                    :background "#32302f"
-                    :foreground "#ebdbb2")
+                    :background "#202020"
+                    :foreground "#D0D0D0")
 (add-to-list 'default-frame-alist '(internal-border-width . 20))
 
 (setq mode-line-format nil)
